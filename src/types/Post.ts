b671cc5e0e7784eec8c55f8de6ef135f880399d1
Nazar Bakay <bakayn484@gmail.com>
2025-10-01 15:22:43 +0300

    add solution

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,7 +1,4 @@
-import { JSX } from "react/jsx-runtime";
-
 export interface Post {
-  map(arg0: (post: Post) => JSX.Element): import("react").ReactNode;
   id: number;
   userId: number;
   title: string;
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,5 +1,4 @@
 export interface User {
-  map: any;
   id: number;
   name: string;
   email: string;
